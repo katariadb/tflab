24b85815074363d90f09d7b5932293d2fec9674b
katariadb <84895602+katariadb@users.noreply.github.com>
2021-05-27 16:26:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "google" {
+   project = "qwiklabs-gcp-02-0661cb755cb3"
+   region= "europe-west2-a"
+}
 resource "google_storage_bucket" "bucket" {
    name = "test"
    location = "europe-west2"
